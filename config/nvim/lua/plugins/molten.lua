a81aa4c187cb9a77554db6439557cd96d4f14f7e
mirza-ahsan <ahsan.17april@gmail.com>
2026-06-22 10:17:41 +0500

    update config.

--- config/nvim/lua/plugins/molten.lua
+++ config/nvim/lua/plugins/molten.lua
@@ -7,9 +7,9 @@ return {
         -- Core settings optimized for your tiling window manager and terminal
         vim.g.molten_image_provider = "image.nvim"
         vim.g.molten_output_win_max_height = 20
-        vim.g.molten_auto_open_output = false
+        vim.g.molten_auto_open_output = true
         vim.g.molten_wrap_output = true
-        vim.g.molten_virt_text_output = true
+        vim.g.molten_virt_text_output = false
         vim.g.molten_virt_lines_off_by_1 = false
     end,
     config = function()
